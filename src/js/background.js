187b28d24fa76f068667efe5d3e46da750090f15
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-03-18 00:32:25 +0900

    Add background page

--- src/js/background.js
+++ src/js/background.js
@@ -4,14 +4,15 @@
 "use strict";
 {
   /* api */
-  const {contextMenus, i18n, pageAction, runtime, tabs} = browser;
+  const {contextMenus, extension, i18n, pageAction, runtime, tabs} = browser;
 
   /* constants */
   const EXT_NAME = "extensionName";
   const HTML_A = "htmlAnchor";
-  const ICON = "./img/icon.svg";
+  const ICON = "img/icon.svg";
   const KEY = "Alt + Shift + C";
   const MD_LINK = "markdownLink";
+  const POPUP = "html/popup.html";
   const TXT_LINK = "textLink";
 
   /**
@@ -32,47 +33,77 @@
   const isString = o => typeof o === "string" || o instanceof String;
 
   /**
+   * stringify positive integer
+   * @param {number} i - integer
+   * @param {boolean} zero - treat 0 as a positive integer
+   * @returns {?string} - stringified integer
+   */
+  const stringifyPositiveInt = (i, zero = false) =>
+    Number.isSafeInteger(i) && (zero && i >= 0 || i > 0) && `${i}` || null;
+
+  /* tabs */
+  /**
    * send data
    * @param {!Object} info - contextMenus.OnClickData
    * @param {!Object} tab - tabs.Tab
    * @returns {Object} - Promise.<?AsyncFunction>
    */
   const sendData = async (info, tab) => {
-    const {id} = tab;
-    const {menuItemId} = info;
-    const data = {info, tab};
-    const input = /\.input$/.test(menuItemId);
-    const msg = {data, input, menuItemId};
-    return id !== tabs.TAB_ID_NONE && tabs.sendMessage(id, msg) || null;
+    const {id: tabId} = tab;
+    let func;
+    if (tabId !== tabs.TAB_ID_NONE) {
+      const {menuItemId} = info;
+      const data = {info, tab};
+      const input = /\.input$/.test(menuItemId);
+      const msg = {data, input, menuItemId};
+      func = tabs.sendMessage(tabId, msg);
+    }
+    return func || null;
   };
 
-  /* active tab info */
-  const actTab = {
-    info: null,
+  /* enabled tabs collection */
+  const enabledTabs = {
+    active: null,
   };
 
   /**
-   * set active tab info
-   * @param {Object} info - active tab info
+   * set enabled tab
+   * @param {number} tabId - tab ID
+   * @param {boolean} enabled - enabled
    * @returns {void} - Promise.<void>
    */
-  const setActiveTab = async (info = {}) => {
-    const {tabId} = info;
-    tabId !== tabs.TAB_ID_NONE && (actTab.info = info);
+  const setEnabledTab = async (tabId, enabled = false) => {
+    if (tabId !== tabs.TAB_ID_NONE) {
+      tabId = stringifyPositiveInt(tabId);
+      tabId && (enabledTabs[tabId] = !!enabled);
+    }
   };
 
   /**
-   * get active tab info
+   * get active tab
    * @returns {Object} - Promise.<Object>, tabs.Tab
    */
   const getActiveTab = async () => {
-    const {info} = actTab;
+    const {active: tabId} = enabledTabs;
     let tab;
-    if (info) {
-      const {tabId} = info;
+    if (Number.isInteger(tabId) && tabId !== tabs.TAB_ID_NONE) {
       tab = await tabs.get(tabId);
     }
     return tab || null;
+  };
+
+  /**
+   * remove enabled tab
+   * @param {number} tabId - tab ID
+   * @returns {boolean} - Promise.<boolean>, result
+   */
+  const removeEnabledTab = async tabId => {
+    let bool;
+    tabId = stringifyPositiveInt(tabId);
+    if (tabId && enabledTabs[tabId]) {
+      bool = delete enabledTabs[tabId];
+    }
+    return bool || false;
   };
 
   /**
@@ -95,36 +126,30 @@
    * @param {Array} contexts - contexts
    * @returns {void} - Promise.<void>
    */
-  const createMenuItem = async (id, contexts) => {
+  const createMenuItem = async (id, contexts, enabled) => {
     isString(id) && Array.isArray(contexts) &&
       contextMenus.create({
         id, contexts,
-        enabled: false,
+        enabled: !!enabled,
         title: i18n.getMessage(id),
       });
   };
 
   /**
    * create context menu items
+   * @param {boolean} enabled - enabled
    * @returns {Object} - Promise.<Array>
    */
-  const createMenuItems = async () => {
+  const createMenuItems = async enabled => {
     const func = [];
     for (const item of menus) {
       func.push(
-        createMenuItem(item, ["all"]),
-        createMenuItem(`${item}.input`, ["all"])
+        createMenuItem(item, ["all"], enabled),
+        createMenuItem(`${item}.input`, ["all"], enabled)
       );
     }
     return Promise.all(func);
   };
-
-  /**
-   * restore context menu
-   * @returns {Object} - Promise.<AsyncFunction>
-   */
-  const restoreContextMenu = async () =>
-    contextMenus.removeAll().then(createMenuItems);
 
   /**
    * update context menu
@@ -145,39 +170,39 @@
   /* page action*/
   /**
    * show icon
+   * @param {number} tabId - tab ID
    * @param {boolean} enabled - enabled
-   * @param {Object} sender - sender info
    * @returns {void} - Promise.<void>
    */
-  const showIcon = async (enabled = false, sender = {}) => {
-    const {tab} = sender;
-    if (tab) {
-      const {id: tabId} = tab;
-      if (tabId !== tabs.TAB_ID_NONE) {
-        if (enabled) {
-          const name = await i18n.getMessage(EXT_NAME);
-          const path = `${ICON}#neutral`;
-          const title = `${name} (${KEY})`;
-          pageAction.setIcon({path, tabId});
-          pageAction.setTitle({tabId, title});
-          pageAction.show(tabId);
-        } else {
-          pageAction.hide(tabId);
-        }
-        setActiveTab({tabId});
+  const showIcon = async (tabId, enabled = false) => {
+    if (Number.isInteger(tabId) && tabId !== tabs.TAB_ID_NONE) {
+      if (enabled) {
+        const name = await i18n.getMessage(EXT_NAME);
+        const icon = await extension.getURL(ICON);
+        const path = `${icon}#neutral`;
+        const popup = await extension.getURL(POPUP);
+        const title = `${name} (${KEY})`;
+        pageAction.setIcon({path, tabId});
+        pageAction.setPopup({popup, tabId});
+        pageAction.setTitle({tabId, title});
+        pageAction.show(tabId);
+      } else {
+        pageAction.hide(tabId);
       }
     }
   };
 
+  /* handlers */
   /**
    * handle message
    * @param {*} msg - message
    * @param {Object} sender - sender
    * @returns {Object} - Promise.<Array>
    */
-  const handleMsg = async (msg, sender) => {
+  const handleMsg = async (msg, sender = {}) => {
     const func = [];
     const items = msg && Object.keys(msg);
+    const tabId = sender && sender.tab && sender.tab.id;
     if (items && items.length) {
       for (const item of items) {
         const obj = msg[item];
@@ -186,7 +211,11 @@
             func.push(updateContextMenu(obj));
             break;
           case "load":
-            func.push(updateContextMenu(obj), showIcon(obj, sender));
+            func.push(
+              setEnabledTab(tabId, obj),
+              showIcon(tabId, obj),
+              updateContextMenu(obj)
+            );
             break;
           case "menuItemId":
             func.push(createData(obj));
@@ -194,6 +223,24 @@
           default:
         }
       }
+    }
+    return Promise.all(func);
+  };
+
+  /**
+   * handle active tab
+   * @param {Object} info - active tab info
+   * @returns {Object} - Promise.<Array>
+   */
+  const handleActiveTab = async (info = {}) => {
+    const {tabId} = info;
+    const func = [];
+    enabledTabs.active = tabId;
+    await contextMenus.removeAll();
+    if (Number.isInteger(tabId) && tabId !== tabs.TAB_ID_NONE) {
+      const enabledTab = await stringifyPositiveInt(tabId);
+      const enabled = enabledTab && enabledTabs[enabledTab] || false;
+      func.push(createMenuItems(enabled), showIcon(tabId, enabled));
     }
     return Promise.all(func);
   };
@@ -206,11 +253,16 @@
     handleMsg(msg, sender).catch(logError)
   );
   tabs.onActivated.addListener(info =>
-    Promise.all([
-      restoreContextMenu(),
-      setActiveTab(info),
-    ]).catch(logError)
+    handleActiveTab(info).catch(logError)
   );
+  tabs.onRemoved.addListener(tabId => {
+    removeEnabledTab(tabId).catch(logError);
+  });
+  tabs.onUpdated.addListener((tabId, info, tab) => {
+    const {active} = tab;
+    const func = active && handleActiveTab({tabId}).catch(logError);
+    return func || null;
+  });
 
   /* startup */
   createMenuItems().catch(logError);
