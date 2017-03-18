ea3b86787b5aa28d57fd4ac159c8ee1e1000476d
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2017-03-19 00:46:20 +0900

    Refactor user input handler (#3)

--- src/js/content.js
+++ src/js/content.js
@@ -7,13 +7,13 @@
   const {i18n, runtime} = browser;
 
   /* constants */
-  const HTML_A = "htmlAnchor";
-  const INPUT = "userInput";
-  const MD_LINK = "markdownLink";
+  const CLIP_TEXT = "clipboardText";
   const NS_HTML = "http://www.w3.org/1999/xhtml";
-  const TXT_LINK = "textLink";
   const TYPE_FROM = 8;
   const TYPE_TO = -1;
+  const USER_INPUT = "userInput";
+  const USER_INPUT_GET = "userInputGet";
+  const USER_INPUT_RES = "userInputRes";
 
   /**
    * log error
@@ -49,6 +49,54 @@
    * @returns {boolean} - result
    */
   const isString = o => typeof o === "string" || o instanceof String;
+
+  /**
+   * send message
+   * @param {*} msg - message
+   * @returns {Object} - Promise.<?AsyncFunction>
+   */
+  const sendMsg = async msg => {
+    const func = msg && runtime.sendMessage(msg);
+    return func || null;
+  };
+
+  /**
+   * send status
+   * @param {!Object} evt - Event
+   * @returns {Object} - Promise.<?AsincFunction>
+   */
+  const sendStatus = async evt => {
+    const enabled = /^(?:(?:(?:application\/(?:[\w\-.]+\+)?|image\/[\w\-.]+\+)x|text\/(?:ht|x))ml)$/.test(document.contentType);
+    const msg = {
+      [evt.type]: enabled,
+    };
+    return enabled && sendMsg(msg) || null;
+  };
+
+  /**
+   * send user input
+   * @param {Object} data - input data
+   * @returns {Object} - Promise.<AsyncFunction>
+   */
+  const sendInput = async (data = {}) => {
+    const msg = {
+      [USER_INPUT_RES]: data,
+    };
+    return sendMsg(msg);
+  };
+
+  /**
+   * get user input
+   * @param {Object} data - input data
+   * @returns {Object} - input data
+   */
+  const getInput = async (data = {}) => {
+    let {content: text} = data;
+    const msg = await i18n.getMessage(USER_INPUT);
+    text = await window.prompt(msg, text);
+    isString(text) && (data.content = text);
+    return data;
+  };
 
   /**
    * copy to clipboard
@@ -99,115 +147,32 @@
   };
 
   /**
-   * get selection text
-   * @param {Object} info - info
-   * @returns {Object} - Promise.<string>
-   */
-  const getSelectionText = async (info = {}) => {
-    let text;
-    if (await getType(info) === "Object" && Object.keys(info).length) {
-      const {selectionText} = info;
-      text = selectionText;
-    } else {
-      const sel = window.getSelection();
-      !sel.isCollapsed && (text = sel.toString());
-    }
-    return text || "";
-  };
-
-  /**
-   * get user input
-   * @param {string} text - default text
-   * @returns {string} - user input text
-   */
-  const getInput = async (text = "") => {
-    const msg = await i18n.getMessage(INPUT);
-    text = await window.prompt(msg, text);
-    return text || "";
-  };
-
-  /**
-   * create HTML Anchor
-   * @param {string} content - content title
-   * @param {string} title - document title
-   * @param {string} url - document URL
-   * @returns {Object} - Promise.<?string>
-   */
-  const createHtml = async (content, title, url) =>
-    isString(content) && isString(title) && isString(url) &&
-    `<a href="${url}" title="${title}">${content.trim()}</a>` || null;
-
-  /**
-   * create Markdown Link
-   * @param {string} content - content title
-   * @param {string} title - document title
-   * @param {string} url - document URL
-   * @returns {Object} - Promise.<?string>
-   */
-  const createMarkdown = async (content, title, url) =>
-    isString(content) && isString(title) && isString(url) &&
-    `[${content.trim()}](${url} "${title}")` || null;
-
-  /**
-   * create Text Link
-   * @param {string} content - content title
-   * @param {string} url - document URL
-   * @returns {Object} - Promise.<?string>
-   */
-  const createText = async (content, url) =>
-    isString(content) && isString(url) && `${content.trim()} <${url}>` || null;
-
-  /**
-   * extract message
+   * handle message
    * @param {*} msg - message
-   * @returns {Object} - Promise.<?AsyncFunction>
+   * @returns {Object} - Promise.<Array>
    */
-  const extractMsg = async (msg = {}) => {
-    const {data, input, menuItemId} = msg;
-    let func;
-    if (data) {
-      const {info, tab} = data;
-      if (tab) {
-        const {title, url} = tab;
-        const selectionText = await getSelectionText(info);
-        const content = input ?
-                          await getInput(selectionText || title) :
-                          selectionText || title;
-        switch (menuItemId) {
-          case HTML_A:
-          case `${HTML_A}.input`:
-            func = createHtml(content, title, url).then(copyToClipboard);
+  const handleMsg = async (msg = {}) => {
+    const items = msg && Object.keys(msg);
+    const func = [];
+    if (items && items.length) {
+      for (const item of items) {
+        const obj = msg[item];
+        switch (item) {
+          case CLIP_TEXT:
+            func.push(copyToClipboard(obj));
             break;
-          case MD_LINK:
-          case `${MD_LINK}.input`:
-            func = createMarkdown(content, title, url).then(copyToClipboard);
-            break;
-          case TXT_LINK:
-          case `${TXT_LINK}.input`:
-            func = createText(content, url).then(copyToClipboard);
+          case USER_INPUT_GET:
+            func.push(getInput(obj).then(sendInput));
             break;
           default:
         }
       }
     }
-    return func || null;
-  };
-
-  /**
-   * send status
-   * @param {!Object} evt - Event
-   * @returns {Object} - Promise.<?AsincFunction>
-   */
-  const sendStatus = async evt => {
-    const enabled = /^(?:(?:(?:application\/(?:[\w\-.]+\+)?|image\/[\w\-.]+\+)x|text\/(?:ht|x))ml)$/.test(document.contentType);
-    const msg = {
-      [evt.type]: enabled,
-    };
-    return enabled && runtime.sendMessage(msg) || null;
+    return Promise.all(func);
   };
 
   /* listeners */
-  runtime.onMessage.addListener(msg => extractMsg(msg).catch(logError));
+  runtime.onMessage.addListener(msg => handleMsg(msg).catch(logError));
   document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener(
       "load", evt => sendStatus(evt).catch(logError), false
