c6f12b88eac48ead6a77a62b13a6105aa0b66df4
asamuzaK (Kazz) <kazz@asamuzak.jp>
2017-09-13 16:31:03 +0900

    Add send context node function

--- src/js/content.js
+++ src/js/content.js
@@ -33,6 +33,39 @@
   };
 
   /**
+   * get active element
+   * @returns {Object} - active element
+   */
+  const getActiveElm = async () => {
+    const sel = window.getSelection();
+    const {anchorNode, focusNode, isCollapsed, rangeCount} = sel;
+    let elm;
+    if (!isCollapsed) {
+      if (anchorNode === focusNode) {
+        if (anchorNode.nodeType === Node.ELEMENT_NODE) {
+          elm = anchorNode;
+        } else {
+          const root = document.documentElement;
+          let parent = anchorNode.parentNode;
+          while (parent && parent.parentNode && parent.parentNode !== root) {
+            if (parent.nodeType === Node.ELEMENT_NODE) {
+              elm = parent;
+              break;
+            }
+            parent = parent.parentNode;
+          }
+        }
+      } else if (rangeCount === 1) {
+        const range = sel.getRangeAt(rangeCount - 1);
+        if (range) {
+          elm = range.commonAncestorContainer;
+        }
+      }
+    }
+    return elm || document.activeElement;
+  };
+
+  /**
    * get anchor element
    * @param {Object} node - element
    * @returns {Object} - anchor element
@@ -115,6 +148,21 @@
   };
 
   /**
+   * send context info
+   * @returns {AsyncFunction} - send message
+   */
+  const sendContextInfo = async () => {
+    const elm = await getActiveElm();
+    const info = await createContextInfo(elm);
+    const msg = {
+      [CONTEXT_INFO]: {
+        contextInfo: info,
+      },
+    };
+    return sendMsg(msg);
+  };
+
+  /**
    * handle message
    * @param {*} msg - message
    * @returns {Promise.<Array>} - results of each handler
@@ -126,11 +174,7 @@
       for (const item of items) {
         switch (item) {
           case CONTEXT_INFO_GET:
-            func.push(sendMsg({
-              [CONTEXT_INFO]: {
-                contextInfo,
-              },
-            }));
+            func.push(sendContextInfo());
             break;
           default:
         }
