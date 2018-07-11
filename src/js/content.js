f3bfe6103b2befde5db34e17c8b0974758169ae7
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2018-07-11 11:39:06 +0900

    Parse canonical url (#52)

--- src/js/content.js
+++ src/js/content.js
@@ -128,7 +128,11 @@
         }
       }
       if (canonical) {
-        contextInfo.canonicalUrl = canonical.getAttribute("href");
+        const {origin} = new URL(document.URL);
+        const url = new URL(canonical.getAttribute("href"), origin);
+        if (url) {
+          contextInfo.canonicalUrl = url.href;
+        }
       }
     }
     return contextInfo;
