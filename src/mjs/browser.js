097764e3716487cfd4136303a0280e76313b9add
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-08-22 16:12:14 +0900

    Refactor browser api handler

--- src/mjs/browser.js
+++ src/mjs/browser.js
@@ -5,10 +5,36 @@
 import {isObjectNotEmpty, isString, throwErr} from "./common.js";
 
 /* api */
-const {management, runtime, storage, tabs, windows} = browser;
+const {
+  commands, management, notifications, permissions, runtime, storage, tabs,
+  windows,
+} = browser;
 
 /* constants */
 const {TAB_ID_NONE} = tabs;
+
+/* commands */
+/**
+ * update command
+ * @param {string} id - command ID
+ * @param {string} value - key value
+ * @returns {void}
+ */
+export const updateCommand = async (id, value = "") => {
+  if (typeof commands.update === "function" &&
+      isString(id) && isString(value)) {
+    const shortcut =
+      value.trim().replace(/\+([a-z])$/, (m, c) => `+${c.toUpperCase()}`);
+    if (/^(?:(?:(?:Alt|Command|(?:Mac)?Ctrl)\+(?:Shift\+)?(?:[\dA-Z]|F(?:[1-9]|1[0-2])|(?:Page)?(?:Down|Up)|Left|Right|Comma|Period|Home|End|Delete|Insert|Space))|F(?:[1-9]|1[0-2]))$/.test(shortcut)) {
+      await commands.update({
+        shortcut,
+        name: id,
+      });
+    } else if (shortcut === "") {
+      await commands.reset(id);
+    }
+  }
+};
 
 /* management */
 /**
@@ -22,11 +48,82 @@ export const getEnabledTheme = async () => {
   return themes;
 };
 
+/* notifications */
+/**
+ * clear notification
+ * @param {string} id - notification ID
+ * @returns {?AsyncFunction} - notifications.clear()
+ */
+export const clearNotification = id => {
+  let func;
+  if (isString(id)) {
+    func = notifications.clear(id).catch(throwErr);
+  }
+  return func || null;
+};
+
+/**
+ * create notification
+ * @param {string} id - notification ID
+ * @param {Object} opt - options
+ * @returns {?AsyncFunction} - notifications.create
+ */
+export const createNotification = async (id, opt) => {
+  let func;
+  if (isString(id) && notifications) {
+    if (notifications.onClosed &&
+        !notifications.onClosed.hasListener(clearNotification)) {
+      notifications.onClosed.addListener(clearNotification);
+    }
+    func = notifications.create(id, opt);
+  }
+  return func || null;
+};
+
+/* permissions */
+/**
+ * remove permission
+ * @param {string|Array} perm - permission
+ * @returns {?AsyncFunction} - permissions.remove
+ */
+export const removePermission = async perm => {
+  let func;
+  if (isString(perm)) {
+    func = permissions.remove({
+      permissions: [perm],
+    });
+  } else if (Array.isArray(perm)) {
+    func = permissions.remove({
+      permissions: perm,
+    });
+  }
+  return func || null;
+};
+
+/**
+ * request permission
+ * @param {string|Array} perm - permission
+ * @returns {?AsyncFunction} - permissions.request
+ */
+export const requestPermission = async perm => {
+  let func;
+  if (isString(perm)) {
+    func = permissions.request({
+      permissions: [perm],
+    });
+  } else if (Array.isArray(perm)) {
+    func = permissions.request({
+      permissions: perm,
+    });
+  }
+  return func || null;
+};
+
 /* runtime */
 /**
  * fetch data
  * @param {string} path - data path
- * @returns {?Object} - JSON data
+ * @returns {Object} - JSON data
  */
 export const fetchData = async path => {
   let data;
@@ -98,6 +195,38 @@ export const setStorage = async obj =>
 
 /* tabs */
 /**
+ * create tab
+ * @param {Object} opt - options
+ * @returns {AsyncFunction} - tabs.create
+ */
+export const createTab = async (opt = {}) =>
+  tabs.create(isObjectNotEmpty(opt) && opt || null);
+
+/**
+ * execute content script to existing tabs
+ * @param {string} src - content script path
+ * @param {boolean} frame - execute to all frames
+ * @returns {Promise.<Array>} - results of each handler
+ */
+export const execScriptToExistingTabs = async (src, frame = false) => {
+  const func = [];
+  if (isString(src)) {
+    const contentScript = runtime.getURL(src);
+    const tabList = await tabs.query({
+      windowType: "normal",
+    });
+    for (const tab of tabList) {
+      const {id: tabId} = tab;
+      func.push(tabs.executeScript(tabId, {
+        allFrames: !!frame,
+        file: contentScript,
+      }));
+    }
+  }
+  return Promise.all(func);
+};
+
+/**
  * get active tab
  * @param {number} windowId - window ID
  * @returns {Object} - tabs.Tab
@@ -162,4 +291,31 @@ export const isTab = async tabId => {
     tab = await tabs.get(tabId).catch(throwErr);
   }
   return !!tab;
+};
+
+/* windows */
+/**
+ * get all windows
+ * @returns {Promise.<Array>} - array of windows.Window
+ */
+export const getAllNormalWindows = async () => windows.getAll({
+  windowTypes: ["normal"],
+});
+
+/**
+ * check whether incognito window exists
+ * @returns {boolean} - result
+ */
+export const checkIncognitoWindowExists = async () => {
+  let incog;
+  const winArr = await getAllNormalWindows();
+  if (winArr && winArr.length) {
+    for (const win of winArr) {
+      incog = win.incognito;
+      if (incog) {
+        break;
+      }
+    }
+  }
+  return !!incog;
 };
