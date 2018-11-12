751666f4776ad868f58427248f3801c911518f42
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-11-12 10:57:28 +0900

    Update test/mocha/setup.js

--- test/mocha/setup.js
+++ test/mocha/setup.js
@@ -1,9 +1,30 @@
 /**
  * setup.js
  */
+
 "use script";
+const {JSDOM} = require("jsdom");
 const browser = require("sinon-chrome/webextensions");
+
+/**
+ * create jsdom
+ * @returns {Object} - jsdom instance
+ */
+const createJsdom = () => {
+  const domstr = "<!DOCTYPE html><html><head></head><body></body></html>";
+  const opt = {
+    runScripts: "dangerously",
+  };
+  return new JSDOM(domstr, opt);
+};
+
+const {window} = createJsdom();
+const {document} = window;
+
 global.browser = browser;
+global.window = window;
+global.document = document;
+
 module.exports = {
   browser,
 };
