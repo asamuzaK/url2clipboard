e5fcd22a67f4b607c2d4ff4a33e2fd05e6ede0c4
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-12-09 17:34:50 +0900

    Create format.test.js

---
+++ test/format.test.js
@@ -0,0 +1,42 @@
+/**
+ * menu-items.test.js
+ */
+/* eslint-disable  max-nested-callbacks, no-await-in-loop, no-magic-numbers */
+
+import {assert} from "chai";
+import {afterEach, beforeEach, describe, it} from "mocha";
+import {browser} from "./mocha/setup.js";
+import format from "../src/mjs/format.js";
+
+describe("format", () => {
+  beforeEach(() => {
+    global.browser = browser;
+  });
+  afterEach(() => {
+    delete global.browser;
+  });
+
+  it("should get browser object", () => {
+    assert.isObject(browser, "browser");
+  });
+
+  describe("keys", () => {
+    const itemKeys = [
+      "HTML", "Markdown", "BBCodeText", "BBCodeURL", "Textile", "AsciiDoc",
+      "MediaWiki", "Jira", "reStructuredText", "LaTeX", "Text",
+    ];
+    const items = Object.entries(format);
+
+    it("should get equal length", () => {
+      assert.isTrue(items.length === itemKeys.length, "length");
+    });
+
+    it("should get string and object", () => {
+      for (const [key, value] of items) {
+        assert.isTrue(itemKeys.includes(key), "item");
+        assert.isString(key, "key");
+        assert.isObject(value, "value");
+      }
+    });
+  });
+});
