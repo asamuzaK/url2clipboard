48fdfa417f5e83f64c305022178202a6382c82bf
asamuzaK (Kazz) <kazz@asamuzak.jp>
2020-06-29 02:56:27 +0900

    Test all channels

--- test/mocha/setup.js
+++ test/mocha/setup.js
@@ -27,7 +27,28 @@ const createJsdom = () => {
 const {window} = createJsdom();
 const {document} = window;
 
-const browser = new Schema("central").mock();
+/**
+ * get channel
+ *
+ * @returns {string} - channel
+ */
+const getChannel = () => {
+  let ch;
+  const reg = /(?<=--channel=)[a-z]+/;
+  const args = process.argv.filter(arg => reg.test(arg));
+  if (args.length) {
+    [ch] = reg.exec(args);
+  } else {
+    ch = "beta";
+  }
+  return ch;
+};
+
+const channel = getChannel();
+
+console.log(`Channel: ${channel}`);
+
+const browser = new Schema(channel).mock();
 
 browser.i18n.getMessage.callsFake((...args) => args.toString());
 browser.permissions.contains.resolves(true);
