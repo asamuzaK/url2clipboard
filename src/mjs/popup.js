5b3f6a133f5bcce8e9722b70edcbedd28e5765e1
asamuzaK (Kazz) <kazz@asamuzak.jp>
2022-09-09 17:26:16 +0900

    Update popup.js

--- src/mjs/popup.js
+++ src/mjs/popup.js
@@ -4,15 +4,15 @@
 
 /* shared */
 import { throwErr } from './common.js';
-import {
-  handleMsg, startup
-} from './popup-main.js';
+import { handleMsg, startup } from './popup-main.js';
 
 /* api */
 const { runtime } = browser;
 
 /* listeners */
-runtime.onMessage.addListener(msg => handleMsg(msg).catch(throwErr));
+runtime.onMessage.addListener((msg, sender) =>
+  handleMsg(msg, sender).catch(throwErr)
+);
 
 /* startup */
 document.addEventListener('DOMContentLoaded', () => startup().catch(throwErr));
