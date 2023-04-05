b800e67e3012f743080f943ae6119b8c4b3e9164
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-04-05 14:02:30 +0900

    Update types

--- types/src/lib/url/url-sanitizer-wo-dompurify.min.d.ts
+++ types/src/lib/url/url-sanitizer-wo-dompurify.min.d.ts
@@ -1,4 +1,4 @@
-declare var y: {
+declare var w: {
     "__#2@#e": number;
     "__#2@#t": Set<any>;
     replace(e: any): any;
@@ -7,6 +7,7 @@ declare var y: {
     parse(e: any, s: any): {
         [k: string]: any;
     };
+    reset(): void;
     "__#1@#e": Set<string>;
     get(): string[];
     has(e: any): boolean;
@@ -14,18 +15,18 @@ declare var y: {
     remove(e: any): boolean;
     verify(e: any): boolean;
 };
-declare function Re(t: any): Promise<boolean>;
+declare function Ue(t: any): Promise<boolean>;
 declare function ve(t: any): boolean;
-declare function Ee(t: any): Promise<{
+declare function xe(t: any): Promise<{
     [k: string]: any;
 }>;
-declare function xe(t: any): {
+declare function Re(t: any): {
     [k: string]: any;
 };
-declare function be(t: any, e?: {
+declare function we(t: any, e?: {
     allow: any[];
     deny: any[];
     only: any[];
 }): Promise<string>;
-declare function we(t: any, e: any): string;
-export { y as default, Re as isURI, ve as isURISync, Ee as parseURL, xe as parseURLSync, be as sanitizeURL, we as sanitizeURLSync };
+declare function Ee(t: any, e: any): string;
+export { w as default, Ue as isURI, ve as isURISync, xe as parseURL, Re as parseURLSync, we as sanitizeURL, Ee as sanitizeURLSync };
---
+++ types/src/mjs/exec-copy.d.ts
@@ -0,0 +1,1 @@
+export function execCopy(opt?: object): void;
