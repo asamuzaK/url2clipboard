3f0328a6307bfb409bacee12da02f20c2ff9b941
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-04-22 09:12:28 +0900

    Update context-info.d.ts

--- types/src/js/context-info.d.ts
+++ types/src/js/context-info.d.ts
@@ -1,4 +1,3 @@
 export function createContextInfo(node?: object): object;
 export function getActiveElm(): object;
-export function getAnchorElm(node?: object): object;
 export function getContextInfo(): object;
