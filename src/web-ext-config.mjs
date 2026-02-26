0eb376baeed80e2a84cc6fef3c958faef9bd5356
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-02-27 01:00:07 +0900

    cjs to mjs

---
+++ src/web-ext-config.mjs
@@ -0,0 +1,12 @@
+export default {
+  ignoreFiles: [
+    './html/offscreen.html',
+    './img/*.png',
+    './js/package.json',
+    './lib/mozilla',
+    './mjs/offscreen.js',
+    './mjs/offscreen-main.js',
+    './repl',
+    'web-ext-config.mjs'
+  ]
+};
