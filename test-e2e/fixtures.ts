80abfe75d662a6dd098722c64156cf5269396575
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-09 12:31:07 +0900

    Add e2e tests

---
+++ test-e2e/fixtures.ts
@@ -0,0 +1,38 @@
+// test-e2e/fixtures.ts
+import { test as base, chromium, type BrowserContext } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const extensionPath = path.join(__dirname, '../bundle');
+
+export const test = base.extend<{
+  context: BrowserContext;
+  extensionId: string;
+}>({
+  context: async ({}, use) => {
+    const context = await chromium.launchPersistentContext('', {
+      headless: false,
+      args: [
+        `--headless=new`,
+        `--disable-extensions-except=${extensionPath}`,
+        `--load-extension=${extensionPath}`,
+      ],
+    });
+
+    await context.grantPermissions(['clipboard-read', 'clipboard-write']);
+    await use(context);
+    await context.close();
+  },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) {
+      background = await context.waitForEvent('serviceworker');
+    }
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
+});
+export const expect = test.expect;
