80a9fa2750cb819e2027e4459477c2e16383a87d
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-05-30 22:31:30 +0900

    Add dompurify hook

--- src/mjs/sanitize-attr.js
+++ src/mjs/sanitize-attr.js
@@ -5,6 +5,13 @@
 /* api */
 const { DOMPurify } = globalThis;
 
+DOMPurify.addHook('uponSanitizeAttribute', (node, data) => {
+  if (data.attrName === 'target') {
+    data.keepAttr = true;
+    data.forceKeepAttr = true;
+  }
+});
+
 DOMPurify.addHook('afterSanitizeAttributes', node => {
   node.removeAttribute('title');
 });
@@ -16,10 +23,13 @@ DOMPurify.addHook('afterSanitizeAttributes', node => {
  */
 export const sanitizeAttributes = (attr = '') => {
   const html = DOMPurify.sanitize(`<a ${attr}></a>`, {
-    ADD_ATTR: ['target'],
+    ADD_ATTR: ['target', 'class', 'rel'],
     ALLOWED_TAGS: ['a'],
     KEEP_CONTENT: false
   });
-  [attr] = html.replace('<a', '').replace('></a>', '').split('<a');
-  return attr.trim();
+  const match = html.match(/^<a([^>]*)>/);
+  if (match && match[1]) {
+    return match[1].replace(/\/$/, '').trim();
+  }
+  return '';
 };
